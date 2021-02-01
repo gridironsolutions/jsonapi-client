67a08f2ba0b80bac39e8a3d6cd9f20dbc52b4bc2
Tom Voss <tom.voss@gridironsolutions.com>
2021-02-01 16:36:39 -0600

    add toString() method

--- src/models/JsonApiResource.js
+++ src/models/JsonApiResource.js
@@ -26,6 +26,20 @@ export default class JsonApiResource extends JsonApiModel {
         }
     }
 
+    toString() {
+        let str = '[object '.concat( this.constructor.name );
+        let id = '';
+
+        // if this object has an id then prepare to append it to the object's class name
+        if ( this.getId() ) {
+            id = '#'.concat( this.getId() );
+        }
+
+        str = str.concat( id ).concat( ']' );
+
+        return str;
+    }
+    
     getType() {
         return this.#type;
     }
