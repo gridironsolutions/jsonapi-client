759242552e9880287c53f78c926cb8b97568f82e
Tom Voss <tom.voss@gridironsolutions.com>
2021-02-01 16:37:10 -0600

    toString() was too specific. update to make it more generic.

--- src/models/JsonApiModel.js
+++ src/models/JsonApiModel.js
@@ -3,16 +3,6 @@
  */
 export default class JsonApiModel {
     toString() {
-        let str = '[object '.concat( this.constructor.name );
-        let id = '';
-
-        // if this object has an id then prepare to append it to the object's class name
-        if ( this.getId() ) {
-            id = '#'.concat( this.getId() );
-        }
-
-        str = str.concat( id ).concat( ']' );
-
-        return str;
+        return '[object '.concat( this.constructor.name ).concat( ']' );
     }
 }
