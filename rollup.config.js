7f007f54862835653f6a235b83e97aaaaf9fed21
Tom Voss <tom.voss@gridironsolutions.com>
2021-02-22 10:47:59 -0600

    switch from webpack to rollup

---
+++ rollup.config.js
@@ -0,0 +1,101 @@
+import del from 'rollup-plugin-delete';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+import {terser} from 'rollup-plugin-terser';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import execute from 'rollup-plugin-execute';
+import acf from 'acorn-class-fields';
+import ascf from 'acorn-static-class-features';
+import apm from 'acorn-private-methods';
+import autoExternal from 'rollup-plugin-auto-external';
+import pkg from "./package.json";
+
+const input = ['src/index.js'];
+const banner =
+`/*
+ * ${pkg.name}
+ * ${pkg.description}
+ * ${pkg.repository.url}
+ * v${pkg.version}
+ * ${pkg.license} License
+ */
+`;
+
+export default [
+    //UMD
+    {
+        input,
+        output: {
+            file: `build/${pkg.name}.min.js`,
+            format: 'umd',
+            name: 'jsonApiClient',
+            esModule: false,
+            exports: 'named',
+            sourcemap: true,
+            banner: banner,
+        },
+        plugins: [
+            del({
+                targets: 'build/*',
+                runOnce: true,
+            }),
+            autoExternal(),
+            nodeResolve({
+                preferBuiltins: true,
+            }),
+            babel({
+                babelHelpers: 'runtime',
+            }),
+            commonjs(),
+            json(),
+            terser(),
+        ],
+    },
+
+    //ESM and CJS
+    {
+        input,
+        output: [
+            //ESM
+            {
+                file: `build/esm/index.js`,
+                format: 'esm',
+                exports: 'named',
+                sourcemap: true,
+                banner: banner,
+            },
+
+            //CJS
+            {
+                file: `build/commonjs/index.js`,
+                format: 'cjs',
+                exports: 'named',
+                sourcemap: true,
+                banner: banner,
+            },
+        ],
+        plugins: [
+            autoExternal(),
+            nodeResolve({
+                preferBuiltins: true,
+            }),
+            babel({
+                babelHelpers: 'runtime',
+            }),
+            commonjs(),
+            json(),
+            terser(),
+            execute([
+                'printf "{\n  \\"type\\": \\"commonjs\\"\n}" > ./build/commonjs/package.json',
+                'yarn version --patch',
+                'cp ./package.json ./build/package.json',
+            ]),
+        ],
+        acornInjectPlugins: [
+            acf,
+            ascf,
+            apm,
+        ]
+    }
+];
