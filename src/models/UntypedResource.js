d82eb800c50fe17694f27078da32131a7a2a6da6
Tom Voss <tom.voss@gridironsolutions.com>
2021-03-09 15:49:55 -0600

    update to new class signature

--- src/models/UntypedResource.js
+++ src/models/UntypedResource.js
@@ -1,7 +1,5 @@
 import JsonApiResource from './JsonApiResource';
 
 export default class UntypedResource extends JsonApiResource {
-    constructor( resource ) {
-        super( resource, 'untypedresource' );
-    }
+    static type = 'untypedresource';
 }
